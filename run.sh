8a39c199718901c9e0c8d898050ec57d78e09cf6
Tudor Berariu <tudor.berariu@gmail.com>
2014-03-17 04:04:28 +0200

    Added pdf file

--- run.sh
+++ run.sh
@@ -25,14 +25,24 @@ get_free_port()
     eval "$1='${Port}'"
 }
 
+launch_round()
+{
+    eval "./bricks_server --port $1 --height $2 --length $3 --output-file $4 &"
+    sleep 1
+    eval "./brickmaker --port $1 --in-file $5 &"
+    eval "./bricklayer.py $1 &"
+}
+
 make build
 
-PORT_SERVER=
-get_free_port PORT_SERVER
+PORT_SERVER_1=
+get_free_port PORT_SERVER_1
+launch_round $PORT_SERVER_1 4 4 score1 distributions/dist1
 
-eval "./bricks_server --port $PORT_SERVER > log &"
-sleep 1
-eval "./brickmaker --port $PORT_SERVER &"
-sleep 1
-eval "./bricklayer.py $PORT_SERVER &"
+PORT_SERVER_2=
+get_free_port PORT_SERVER_2
+launch_round $PORT_SERVER_2 8 5 score2 distributions/dist2
 
+PORT_SERVER_3=
+get_free_port PORT_SERVER_3
+launch_round $PORT_SERVER_3 8 6 score3 distributions/dist3
